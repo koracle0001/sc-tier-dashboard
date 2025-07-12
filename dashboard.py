843493c92eb18a2c3a88f2744e5455ec6f53745b
koracle0001 <stpaulo4932@gmail.com>
2025-07-12 23:09:20 +0900

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -224,17 +224,17 @@ with col2:
 with col3:
     st.markdown("#### ℹ️ 지표 설명")
     st.markdown("""
-    <div style="background-color: #e6f3ff; border-left: 5px solid #1a8cff; padding: 10px; border-radius: 5px; margin: 10px 0;">
-        <ul style="list-style-type: none; padding-left: 0;">
-            <li style="margin-bottom: 10px;">
+    <div style="background-color: #e6f3ff; border-left: 5px solid #1a8cff; padding: 10px; border-radius: 5px; margin: 10px 0 5px 0;">
+        <ul style="list-style-type: none; padding-left: 0; margin-bottom: 0;">
+            <li style="margin-bottom: 8px;">
                 <strong>이레귤러</strong>: 특정상황에서 티어 내 강자를 의미<br>
                 <span style="font-size: 0.9em;">(티어 내 순위와는 무관)</span>
             </li>
-            <li style="margin-bottom: 10px;">
+            <li style="margin-bottom: 8px;">
                 <strong>클러치</strong>: 스폰 게임 대비 중요 경기 기대 승률<br>
                 <span style="font-size: 0.9em;">(높을 수록 큰 경기에 강함)</span>
             </li>
-            <li>
+            <li style="margin-bottom: 8px;">
                 <strong>로직 신뢰도 등급</strong>: 판정 결과의 유효 신뢰도<br>
                 <span style="font-size: 0.9em;">(플레이어의 점수에는 일절 영향을 주지 않음)</span>
             </li>
@@ -248,15 +248,15 @@ with col3:
 
     st.markdown("#### ⚙️ 개선 작업중")
     st.markdown("""
-    <div style="background-color: #f0fff0; border-left: 5px solid #28a745; padding: 12px; border-radius: 5px; margin: 10px 0;">
-        <h5 style="margin-top: 0; margin-bottom: 9px; font-weight: bold;">(8월 내 적용예정, 테스트 중)</h5>
+    <div style="background-color: #f0fff0; border-left: 5px solid #28a745; padding: 10px; border-radius: 5px; margin: 5px 0;">
+        <h5 style="margin-top: 0; margin-bottom: 8px; font-weight: bold;">(8월 내 적용예정, 테스트 중)</h5>
         <ul style="list-style-type: none; padding-left: 5px; margin-bottom: 0;">
-            <li style="margin-bottom: 5px;"> 특정 플레이어의 상세로그 공개에 관한 프로세스를 준비중입니다.</li>
-            <li> 요청이 많은 순서대로 공개할 수 있도록 하겠습니다.</li>
+            <li style="margin-bottom: 4px;">- 특정 플레이어의 상세로그 공개에 관한 프로세스를 준비중입니다.</li>
+            <li>- 요청이 많은 순서대로 공개할 수 있도록 하겠습니다.</li>
         </ul>
     </div>
     """, unsafe_allow_html=True)
-
+    
 # --- 요약 통계 ---
 st.divider()
 st.header('📊 요약 통계')
